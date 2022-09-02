1a37e5c4f591e25b636ac4a4ced035ff501377b6
TIRTHESH MOTIWALA <tirtheshmotiwala@gmail.com>
2022-09-02 22:37:21 +0530

    My stack Overflow clone

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ import questionRoutes from './routes/Question.js'
 import answerRoutes from './routes/Answers.js'
 import dotenv from 'dotenv'
 
-dotenv.config();
 const app = express()
+dotenv.config();
 app.use(express.json({limit:"30mb",extended:true}))
 app.use(express.urlencoded({limit:"30mb",extended:true}))
 app.use(cors());
@@ -20,10 +20,10 @@ app.use('/user',userRoutes)
 app.use('/questions',questionRoutes)
 app.use('/answer',answerRoutes)
 
-const PORT = process.env.PORT || 5000
+//const PORT = process.env.PORT || 5000
 
-const CONNECTION_URL = "mongodb+srv://admin:admin@stack-overflow-clone.23q8cpa.mongodb.net/?retryWrites=true&w=majority"
 const DATABASE_URL = process.env.CONNECTION_URL
-mongoose.connect(DATABASE_URL,{useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT,() => {console.log(`server running on port ${PORT}`)}))
-    .catch((err) => console.log(err.message))
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+        .then(() => app.listen(process.env.PORT || 5000, () => {console.log(`Server running on port ${PORT}`)}))
+        .catch((err) => console.log(err.message))
